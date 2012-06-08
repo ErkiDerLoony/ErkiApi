5a33ab9939aadcc9c95312f340372799556dfee8
Edgar Kalkowski <eMail@edgar-kalkowski.de>
2012-06-08 13:31:49 +0200

    Adjust search path.

--- cmake/Findliberkiapi.cmake
+++ cmake/Findliberkiapi.cmake
@@ -1,6 +1,7 @@
 find_path(liberkiapi_INCLUDES
   NAMES Plot2D.hpp
-  PATHS erkiapi/plot/
+  PATHS /usr/include/erkiapi/plot
+        /usr/local/include/erkiapi/plot
 )
 
 find_library(liberkiapi_LIBRARIES
@@ -9,22 +10,22 @@ find_library(liberkiapi_LIBRARIES
 )
 
 if(liberkiapi_INCLUDES)
-  message("Found ErkiApi includes.")
+  message(STATUS "Found ErkiApi includes at ${liberkiapi_INCLUDES}.")
 else()
   if(liberkiapi_FIND_REQUIRED)
     message(FATAL_ERROR "Could not find ErkiApi includes!")
   else()
-    message("Could not find ErkiApi includes!")
+    message(STATUS "Could not find ErkiApi includes!")
   endif()
 endif()
 
 if(liberkiapi_LIBRARIES)
-  message("Found ErkiApi libraries.")
+  message(STATUS "Found ErkiApi libraries at ${liberkiapi_LIBRARIES}.")
   set(liberkiapi_FOUND TRUE)
 else()
   if(liberkiapi_FIND_REQUIRED)
     message(FATAL_ERROR "Could not find ErkiApi libraries!")
   else()
-    message("Could not find ErkiApi libraries.")
+    message(STATUS "Could not find ErkiApi libraries.")
   endif()
 endif()
